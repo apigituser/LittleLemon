3ed8e5c03cd4e16e45c787f0f6ab6f03405dfd46
apigituser <responsemail850@gmail.com>
2025-03-25 20:55:58 +0530

    added router path for booking

--- restaurant/models.py
+++ restaurant/models.py
@@ -5,7 +5,7 @@ class Booking(models.Model):
     no_of_guests = models.IntegerField()
     bookingDate = models.DateTimeField()
 
-class Menu(models.Model):
+class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=2, max_digits=10)
     inventory = models.IntegerField()
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import View
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
